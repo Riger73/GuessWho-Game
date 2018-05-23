28121132156e0e3affe72ec8abd2464930d45c0a
Riger73 <s3572290@student.rmit.edu.au>
2018-05-23 15:19:43 +1000

    Update Person.java

--- Person.java
+++ Person.java
@@ -2,25 +2,30 @@ public class Person{
 
 	protected String name;
 	protected String hairLength;
-	protected String gender;
-	protected String eyeColour;
-	protected String headWear;
-	protected String hairColour;
-	protected String origin;
-	protected String ageGroup;
+	protected String glasses;
+	protected String facialHair;
+	protected String eyeColor;
+	protected String pimples;
+	protected String hat;
+	protected String hairColor;
+	protected String noseShape;
+	protected String faceShape;
 	protected boolean isDown;
 	
-	public Person(String name, String hairLength, String gender, String eyeColour, String headWear, String hairColour, String origin, String ageGroup){
+	public Person(String name, String hairLength, String glasses, String facialHair, String eyeColor, String pimples, String hat, String
+		hairColor, String noseShape, String faceShape){
 
 		// Attributes for the character;
 		this.name = name;
 		this.hairLength = hairLength;
-		this.gender = gender;
-		this.eyeColour = eyeColour;
-		this.headWear = headWear;
-		this.hairColour = hairColour;
-		this.origin = origin;
-		this.ageGroup = ageGroup;
+		this.glasses = glasses;
+		this.facialHair = facialHair;
+		this.eyeColor = eyeColor;
+		this.pimples = pimples;
+		this.hat = hat;
+		this.hairColor = hairColor;
+		this.noseShape = noseShape;
+		this.faceShape = faceShape;
 	
 		// Variables to check if down or up;
 		boolean isDown = false;
@@ -42,18 +47,22 @@ public class Person{
 				return name;
 			case "hairLength":
 				return hairLength;
-			case "gender":
-				return gender;
-			case "eyeColour":
-				return eyeColour;
-			case "headWear":
-				return headWear;
-			case "hairColour":
-				return hairColour;
-			case "origin":
-				return origin;
-			case "ageGroup":
-				return ageGroup;
+			case "glasses":
+				return glasses;
+			case "facialHair": 
+				return facialHair;
+			case "eyeColor":
+				return eyeColor;
+			case "pimples":
+				return pimples;
+			case "hat":
+				return hat;
+			case "hairColor":
+				return hairColor;
+			case "noseShape":
+				return noseShape;
+			case "faceShape":
+				return faceShape;
 		}
 
 		return "Unknown";
